aa9f91c2b32202722a09fa71ed72c7f9304db4f1
Ensick <fede.censi96@gmail.com>
2022-12-14 13:00:00 +0100

    Inizio Esercizio - Header Completato

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+
+    <header>
+        <div class="user">
+            <div class="user-left">
+                <img src="./assets/img/google-logo.png" alt="logo">
+                <span>Privacy e Termini</span>
+            </div>
+            <div class="user-right">
+                <i class="fa-solid fa-list-ul"></i>
+                <div>
+                    <span>F</span>
+                </div>
+            </div>
+        </div>
+        
+        <nav>
+            <ul class="nav-menu">
+                <li>Introduzione</li>
+                <li>Norme sulla privacy</li>
+                <li>Termini di servizio</li>
+                <li>Tecnologie</li>
+                <li>Domande frequenti</li>
+            </ul>
+        </nav>
+    </header>
+    
+    <main>
+        <section>
+                <?php 
+                
+
+                ?>
+        </section>
+    </main>
+    
+    
+</body>
+</html>
